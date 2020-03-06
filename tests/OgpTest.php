f4214a0b2191f7e81fe7be09e8bf04cd453618a3
rozeo <st.prog0908@gmail.com>
2020-03-06 16:12:21 +0900

    add ogp test

---
+++ tests/OgpTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PHPUnit\Framework\TestCase;
+
+use Rozeo\Ogp\Ogp;
+use Rozeo\Ogp\OgpDataInterface;
+
+class OgpTest extends TestCase
+{
+    private $imageMock;
+
+    public function setUp(): void
+    {
+        $this->imageMock = new class implements OgpDataInterface
+        {
+            public function toHtml()
+            {
+                return "";
+            }
+        };
+    }
+
+    /**
+     * @test
+     */
+    public function checkConstruct()
+    {
+        $title = "Hey Title";
+        $type = "hogeType";
+        $url = "http://example.com/uniq";
+        $instance = new Ogp($title, $type, $this->imageMock, $url);
+
+        $this->assertEquals($instance->getTitle(), $title);
+        $this->assertEquals($instance->getType(), $type);
+        $this->assertEquals($instance->getOgpDataBody(), $this->imageMock);
+        $this->assertEquals($instance->getUrl(), $url);
+    }
+}
